75acf4780baca4b1869af56ea4ee16767ecb2a0f
Yuka Langbuana <langbuana.yuka@hotmail.com>
2019-04-21 23:27:41 -0700

    fixing heroku database url (again)

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 #APP
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = $(heroku config:get DATABASE_URL -a saudagar-core)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://jbdspjaddeofzc:8f2c92ef9ac427ef0274417ccc235e6d420fe4ff57be9f009b89ddb4f06dedd5@ec2-54-225-113-7.compute-1.amazonaws.com:5432/d1v6cjh3kibd5f"
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yukalangbuana:yukalangbuana@localhost:5432/asiamajor' #====> LOCAL DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
