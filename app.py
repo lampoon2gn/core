95f5ca6e4c48361e8dcdb3427ed62505634ca69e
Yuka Langbuana <langbuana.yuka@hotmail.com>
2019-04-21 23:05:18 -0700

    Deploying saudagar-core V3 to Heroku

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
 #APP
-app = Flask(__name__, template_folder='templates')
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://epbwbeeuzkuhwa:a5c4009f63a95fa6266d17e27dc5b600dd716dbed12b7585cbdcb28961fcb0dc@ec2-54-225-113-7.compute-1.amazonaws.com:5432/d1v6cjh3kibd5f'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yukalangbuana:yukalangbuana@localhost:5432/asiamajor' #====> LOCAL DATABASE
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://epbwbeeuzkuhwa:a5c4009f63a95fa6266d17e27dc5b600dd716dbed12b7585cbdcb28961fcb0dc@ec2-54-225-113-7.compute-1.amazonaws.com:5432/d1v6cjh3kibd5f'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yukalangbuana:yukalangbuana@localhost:5432/asiamajor' #====> LOCAL DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #API
